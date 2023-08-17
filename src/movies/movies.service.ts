2bde5cd4639e945032219b6d6835a74db7fc5b7a
KKardd <wlstjd4013@gmail.com>
2023-08-17 15:30:20 +0900

    feat: add not found exception

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Movie } from './entities/movie.entity';
 
 @Injectable()
@@ -10,14 +10,18 @@ export class MoviesService {
   }
 
   getOne(id: string): Movie {
-    return this.movies.find((movie) => movie.id === Number(id));
+    const movie = this.movies.find((movie) => movie.id === Number(id));
+    if (!movie) {
+      throw new NotFoundException(`Movie with ID: ${id} not found.`);
+    }
+    return movie;
   }
 
-  deleteOne(id: string): boolean {
-    this.movies.filter((movie) => {
+  deleteOne(id: string) {
+    this.getOne(id);
+    this.movies = this.movies.filter((movie) => {
       movie.id !== Number(id);
     });
-    return true;
   }
 
   create(movieData): any {
@@ -25,5 +29,11 @@ export class MoviesService {
       id: this.movies.length + 1,
       ...movieData,
     });
+  }
+
+  update(id: string, updateData) {
+    const movie = this.getOne(id);
+    this.deleteOne(id);
+    this.movies.push({ ...movie, ...updateData });
   }
 }
